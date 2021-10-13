52ca5d6944847b9f7d3a7e2f627eabd3d600f2e6
Junjie Gao <jungao@student.unimelb.edu.au>
2021-10-13 14:25:03 +0800

    ssl

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
-const PORT = 80;
+const PORT = 443;
 
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
+const privateKey = fs.readFileSync('privatekey.pem');
+const certificate = fs.readFileSync('certificate.pem');
 
 const app = express();
 
@@ -9,6 +13,15 @@ app.use(express.static(path.join(__dirname, '../build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/../build/index.html'));
 });
-app.listen(PORT);
+
+https
+  .createServer(
+    {
+      key: privateKey,
+      cert: certificate,
+    },
+    app
+  )
+  .listen(port);
 
 console.log(`running the server on port ${PORT}`);
