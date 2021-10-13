52c8457b093ac1f05fd521abeb53e55f6096166b
Junjie Gao <jungao@student.unimelb.edu.au>
2021-10-13 14:29:33 +0800

    fix pathname again

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const privateKey = fs.readFileSync('simempire.fun.pem');
+const privateKey = fs.readFileSync('simempire.fun.key');
 const certificate = fs.readFileSync('simempire.fun.pem');
 
 const app = express();
