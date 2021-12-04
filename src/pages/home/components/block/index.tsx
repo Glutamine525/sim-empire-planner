40515d38eec011ca386fb9a1bba5ef37d6659d22
Junjie Gao <jungao@student.unimelb.edu.au>
2021-12-05 04:15:32 +0800

    home page map filter

--- src/pages/home/components/block/index.tsx
+++ src/pages/home/components/block/index.tsx
@@ -1,5 +1,5 @@
-import { Dropdown, Menu, Switch } from 'antd';
-import React, { FC, useState } from 'react';
+import { Dropdown, Empty, Menu, Switch } from 'antd';
+import React, { FC, useMemo, useState } from 'react';
 
 import { CivilType } from '@/types/civil';
 
@@ -22,7 +22,7 @@ const data = [
   },
   {
     id: '2',
-    woodNum: 5,
+    woodNum: 4,
     civil: CivilType.China,
     isNoWood: false,
     like: 10,
@@ -35,7 +35,7 @@ const data = [
   },
   {
     id: '3',
-    woodNum: 5,
+    woodNum: 4,
     civil: CivilType.China,
     isNoWood: false,
     like: 10,
@@ -50,7 +50,7 @@ const data = [
     id: '4',
     woodNum: 5,
     civil: CivilType.China,
-    isNoWood: false,
+    isNoWood: true,
     like: 10,
     liked: true,
     uploadTime: new Date().getTime() - 200000,
@@ -135,6 +135,26 @@ const Block: FC<BlockProps> = props => {
   const { type, title } = props;
 
   const [mapType, setMapType] = useState('全部');
+  const [isNoWood, setIsNoWood] = useState('全部');
+  const [editable, setEditable] = useState(false);
+
+  const selectedData = useMemo(() => {
+    let selectedData = data.slice();
+    if (['5', '4', '3'].includes(mapType.split('')[0])) {
+      selectedData = selectedData.filter(
+        v => v.woodNum === Number(mapType.split('')[0])
+      );
+    }
+    if (isNoWood === '无木') {
+      selectedData = selectedData.filter(v => v.isNoWood);
+    } else if (isNoWood === '有木') {
+      selectedData = selectedData.filter(v => !v.isNoWood);
+    }
+    if (editable) {
+      selectedData = selectedData.filter(v => v.editable);
+    }
+    return selectedData;
+  }, [mapType, isNoWood, editable]);
 
   const mapTypeDropdown = (
     <Menu
@@ -152,41 +172,62 @@ const Block: FC<BlockProps> = props => {
     </Menu>
   );
 
+  const isNoWoodDropdown = (
+    <Menu
+      style={{
+        width: '4.8rem',
+        textAlign: 'center',
+      }}
+      onClick={item => {
+        setIsNoWood(item.key);
+      }}
+    >
+      {['全部', '有木', '无木'].map(v => {
+        return <Menu.Item key={v}>{v}</Menu.Item>;
+      })}
+    </Menu>
+  );
+
   return (
     <div className={styles.container}>
       <div className={styles['title-container']}>
         <div className={styles.title}>
           {title}
-          <span className={styles.count}>10</span>
+          <span className={styles.count}>{selectedData.length}</span>
         </div>
         {type === 'common' && (
           <div className={styles.filter}>
             <div>
               <div className={styles['filter-title']}>地图类型</div>
               <Dropdown overlay={mapTypeDropdown} placement="bottomCenter">
-                <span
-                  className={`ant-dropdown-link ${styles['map-type-label']}`}
-                >
-                  {mapType}
-                </span>
+                <span className={styles.label}>{mapType}</span>
               </Dropdown>
             </div>
             <div>
               <div className={styles['filter-title']}>无木之地</div>
-              <Switch size="small" />
+              <Dropdown overlay={isNoWoodDropdown} placement="bottomCenter">
+                <span className={styles.label}>{isNoWood}</span>
+              </Dropdown>
             </div>
             <div>
               <div className={styles['filter-title']}>可编辑</div>
-              <Switch size="small" />
+              <Switch
+                size="small"
+                checked={editable}
+                onClick={checked => {
+                  setEditable(checked);
+                }}
+              />
             </div>
           </div>
         )}
       </div>
       <div className={styles.content}>
-        {data.map(v => (
+        {selectedData.map(v => (
           <Displayer {...v} key={v.id} type={type} />
         ))}
       </div>
+      {!selectedData.length && <Empty description={<span>暂无数据</span>} />}
     </div>
   );
 };
